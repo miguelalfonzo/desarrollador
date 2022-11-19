a92837cd11112fd8423bacecb52af46d7d536e43
Miguel <miguel_94_14@outlook.com>
2022-11-19 18:51:24 -0500

    subiendo landing

---
+++ assets/js/transition.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.zoom').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
+    });
+
+
+    
+
+      
+
+});
---
+++ html/test.php
@@ -0,0 +1,10 @@
+<?php 
+
+$var1=null;
+
+if(trim($var1)==null || trim($var1)==""){
+
+	echo 'entra';
+}
+
+ ?>
